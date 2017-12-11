eb102e99307640175df7dfbe565d7be9bb91c8d0
Ilkka Lehikoinen <ilkka.lehikoinen@helsinki.fi>
2017-12-11 13:32:08 +0200

    fix to heroku authentication part3

--- src/main/java/wad/profiles/SecurityProductionConfiguration.java
+++ src/main/java/wad/profiles/SecurityProductionConfiguration.java
@@ -8,6 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 @Profile("production")
 @Configuration
 @EnableWebSecurity
